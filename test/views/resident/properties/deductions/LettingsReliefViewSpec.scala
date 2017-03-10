ea66f488176741c33b61163b16e2c5f7802b08cb
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2017-03-10 11:21:27 +0000

    Updated headers to reflect the correct copyright year of 2017

--- test/views/resident/properties/deductions/LettingsReliefViewSpec.scala
+++ test/views/resident/properties/deductions/LettingsReliefViewSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -155,5 +155,3 @@ class LettingsReliefViewSpec extends UnitSpec with WithFakeApplication with Fake
     }
   }
 }
-
-
