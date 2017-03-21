77d145c3de374e3e5726a9ce613a8cf2b59f6dbc
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2017-03-21 10:49:17 +0000

    Remove customer filter sequence

--- app/config/frontendGlobal.scala
+++ app/config/frontendGlobal.scala
@@ -44,9 +44,6 @@ object FrontendGlobal
     ApplicationCrypto.verifyConfiguration()
   }
 
-  override protected def defaultFrontendFilters: Seq[EssentialFilter] =
-    super.defaultFrontendFilters.filterNot(filter => filter.isInstanceOf[SessionTimeoutFilter])
-
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit rh: Request[_]): Html = {
     val url = """^(.*[\/])""".r findFirstIn rh.path
     val homeNavLink = url match {
