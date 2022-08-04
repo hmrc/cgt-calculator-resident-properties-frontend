1cbc4210c169a1a01c52177e458fff47f1e26e48
Pankaj Khare <12529426+pankaj2453@users.noreply.github.com>
2022-08-04 10:33:57 +0100

    merged with DL-5860

--- test/views/resident/properties/whatNext/WhatNextSAGainViewSpec.scala
+++ test/views/resident/properties/whatNext/WhatNextSAGainViewSpec.scala
@@ -50,11 +50,11 @@ class WhatNextSAGainViewSpec extends CommonPlaySpec with WithCommonFakeApplicati
       }
 
       s"has a first bullet point of ${pageMessages.bulletPointOne("2016 to 2017")}" in {
-        doc.body.select("#main-content > div > div > ul > li:nth-child(1)").text() shouldBe pageMessages.bulletPointOne("2016 to 2017")
+        doc.select("ul.govuk-list li").get(0).text shouldBe pageMessages.bulletPointOne("2016 to 2017")
       }
 
       s"has a second bullet point of ${pageMessages.bulletPointTwo}" in {
-        doc.body.select("#main-content > div > div > ul > li:nth-child(2)").text() shouldBe pageMessages.bulletPointTwo
+        doc.select("ul.govuk-list li").get(1).text shouldBe pageMessages.bulletPointTwo
       }
     }
 
