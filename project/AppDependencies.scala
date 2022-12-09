0fabb2b4165cffebef538b0b9afcc988b5ec13d6
AIbrahimQA <57941383+AIbrahimQA@users.noreply.github.com>
2022-12-09 12:13:30 +0000

    Remove unused mongo from AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -26,7 +26,6 @@ object AppDependencies {
   val govUKTemplateVersion     = "5.77.0-play-28"
   val playPartialsVersion      = "8.3.0-play-28"
   val httpCachingClientVersion = "9.6.0-play-28"
-  val mongoCachingVersion      = "7.1.0-play-28"
   val playLanguageVersion      = "5.3.0-play-28"
   val play2PdfVersion          = "1.10.0"
 
@@ -38,7 +37,6 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "govuk-template"       % govUKTemplateVersion,
     "uk.gov.hmrc"       %% "play-partials"        % playPartialsVersion,
     "uk.gov.hmrc"       %% "http-caching-client"  % httpCachingClientVersion,
-    "uk.gov.hmrc"       %% "mongo-caching"        % mongoCachingVersion,
     "uk.gov.hmrc"       %% "play-language"        % playLanguageVersion,
     "it.innove"         %  "play2-pdf"            % play2PdfVersion exclude("com.typesafe.play","*")
   )
