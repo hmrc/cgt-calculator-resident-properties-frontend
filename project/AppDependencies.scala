f6120350c49907ea4435e763b33e6d9e71b8f6d7
HG-Accenture <183600681+HG-Accenture@users.noreply.github.com>
2025-01-13 14:52:05 +0000

    DLS-10859 - cleanup.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -40,12 +40,6 @@ object AppDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc.mongo"       %%  s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion                 % scope,
         "uk.gov.hmrc"             %% s"bootstrap-test-$playVersion"   % bootstrapVersion                 % scope,
-        "org.jsoup"               %  "jsoup"                          % "1.18.3"                         % scope,
-        "org.scalatestplus"       %% "scalatestplus-mockito"          % "1.0.0-SNAP5"                       % scope,
-        "org.scalatestplus"       %% "scalatestplus-scalacheck"       % "3.1.0.0-RC2"                    % scope,
-        "org.scalatestplus.play"  %% "scalatestplus-play"             % "7.0.1"                          % scope,
-        "org.mockito"             %% "mockito-scala-scalatest"        % "1.17.37"                        % scope,
-        "com.github.tomakehurst"  % "wiremock"                        % "3.0.0-beta-7"                   % scope
       )
     }.test
   }
